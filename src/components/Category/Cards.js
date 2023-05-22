43a2e18ba07e2095b3ce775f2ae7f15f08398c12
Abenetwolde <tecnoabi4@gmail.com>
2023-05-22 13:50:40 -0700

    update card

--- src/components/Category/Cards.js
+++ src/components/Category/Cards.js
@@ -1,25 +1,13 @@
 import React from 'react'
 import { Box, Grid } from "@mui/material";
 import SummaryCard from './SummaryCard';
+import categorys from "../../data/category.json"
 function Cards() {
   return (
     <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" m="2.5rem 10rem">
     <Grid container spacing={2}>
-      <Grid item xs={12} sm={6} md={3}>
-        <SummaryCard title="Restaurant " total={71} icon={'material-symbols:restaurant'} />
-      </Grid>
 
-      <Grid item xs={12} sm={6} md={3}>
-        <SummaryCard title="Services" total={40} icon={'material-symbols:home-repair-service'} />
-      </Grid>
-
-      <Grid item xs={12} sm={6} md={3}>
-        <SummaryCard title="Shop" total={4} icon={'material-symbols:shopping-cart'} />
-      </Grid>
-
-      <Grid item xs={12} sm={6} md={3}>
-        <SummaryCard title="Others" total={90} icon={'material-symbols:add-box-outline-rounded'} />
-      </Grid>
+        {categorys.MainCategories.map((main)=><Grid item xs={12} sm={6} md={3}><SummaryCard title={main.name} total={main.subcategories.length} icon={main.image} /> </Grid>)}
     </Grid>
   </Box>
   )
