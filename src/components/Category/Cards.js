a8e17483befe04ad915f014ffdce89567b996582
Abenetwolde <tecnoabi4@gmail.com>
2023-05-22 14:20:16 -0700

    update card2

--- src/components/Category/Cards.js
+++ src/components/Category/Cards.js
@@ -5,11 +5,10 @@ import categorys from "../../data/category.json"
 function Cards() {
   return (
     <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" m="2.5rem 10rem">
-    <Grid container spacing={2}>
-
-        {categorys.MainCategories.map((main)=><Grid item xs={12} sm={6} md={3}><SummaryCard title={main.name} total={main.subcategories.length} icon={main.image} /> </Grid>)}
-    </Grid>
-  </Box>
+      <Grid container spacing={2}>
+        {categorys.MainCategories.map((main) => <Grid item xs={12} sm={6} md={3}><SummaryCard title={main.name} total={main.subcategories.length} icon={main.image} /> </Grid>)}
+      </Grid>
+    </Box>
   )
 }
 
